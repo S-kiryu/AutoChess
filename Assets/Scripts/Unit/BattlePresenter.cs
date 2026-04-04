67e006152dd1b58bbb0431cf585ebee4b24fbc48
KINOKO\nubob <Ki28ryu@icloud.com>
2026-04-05 08:42:30 +0900

    [add]攻撃対象を指定して攻撃するクラスの作成

---
+++ Assets/Scripts/Unit/BattlePresenter.cs
@@ -0,0 +1,40 @@
+public class BattlePresenter
+{
+    private UnitModel attacker;
+    private UnitModel target;
+
+    private UnitView attackerView;
+    private UnitView targetView;
+
+    public BattlePresenter(
+        UnitModel attacker,
+        UnitModel target,
+        UnitView attackerView,
+        UnitView targetView)
+    {
+        this.attacker = attacker;
+        this.target = target;
+        this.attackerView = attackerView;
+        this.targetView = targetView;
+
+        UpdateView();
+    }
+
+    public void Attack()
+    {
+        var attackData = new AttackData(attacker.Attack, DamageType.Physical);
+
+        float damage = DamageCalculator.CalculateDamage(attacker, attackData, target);
+
+        target.TakeDamage(damage);
+
+        UpdateView();
+    }
+
+    //Uiの更新を行うメソッド
+    private void UpdateView()
+    {
+        attackerView.SetHP(attacker.CurrentHp);
+        targetView.SetHP(target.CurrentHp);
+    }
+}

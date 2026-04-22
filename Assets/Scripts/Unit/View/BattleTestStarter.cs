1fd8eff5bd2053ea48adb274634ba68b933e84f5
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-04-22 13:06:06 +0900

    [add]バトルテストシステム

---
+++ Assets/Scripts/Unit/View/BattleTestStarter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BattleTestStarter : MonoBehaviour
+{
+    [SerializeField] private UnitPresenter unitPresenter;
+    [SerializeField] private BaseStatus allyStatus;
+    [SerializeField] private BaseStatus enemyStatus;
+    [SerializeField] private UnitView allyView;
+    [SerializeField] private UnitView enemyView;
+
+    private void Start()
+    {
+        UnitModel ally = new UnitModel(allyStatus, UnitModel.TeamType.Player);
+        UnitModel enemy = new UnitModel(enemyStatus, UnitModel.TeamType.Enemy);
+
+        unitPresenter.RegisterUnit(ally, allyView, new Vector2Int(0, 0));
+        unitPresenter.RegisterUnit(enemy, enemyView, new Vector2Int(4, 0));
+    }
+}

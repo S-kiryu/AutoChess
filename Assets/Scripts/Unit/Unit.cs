bad1b764a2a74e4fb18f5d3fe30444bfaf996975
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-04-25 20:20:05 +0900

    [add]ユニットが持つべきデータをまとめたクラス

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using static UnitModel;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField] private TeamType _teamType;
+    [SerializeField] private BaseStatus _baseStatus;
+    [SerializeField] private UnitView _unitView;
+
+    public UnitModel UnitModel { get; private set; }
+
+    public TeamType TeamType => _teamType;
+    public BaseStatus BaseStatus => _baseStatus;
+    public UnitView UnitView => _unitView;
+
+    private void Awake()
+    {
+        UnitModel = new UnitModel(_baseStatus, _teamType);
+    }
+}

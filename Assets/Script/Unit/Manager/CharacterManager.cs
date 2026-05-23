6d7cd601657a9e4d9dcb9fb84fb78a19c16c8971
KINOKO\nubob <Ki28ryu@icloud.com>
2026-05-23 14:28:13 +0900

    [add]キャラ全体を管理するクラス

---
+++ Assets/Script/Unit/Manager/CharacterManager.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterManager : MonoBehaviour
+{
+    public static CharacterManager Instance { get; private set; }
+
+    private List<UnitInstance> _characters = new List<UnitInstance>();
+    public List<UnitInstance> Charactrs => _characters;
+    private void Awake()
+    {
+        //if (Instance != null && Instance != this)
+        //{
+        //    Destroy(gameObject);
+        //    return;
+        //}
+        //DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+
+}

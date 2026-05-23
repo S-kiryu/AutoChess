014e1df3e0581e2d350ca81955f143cf529d1928
KINOKO\nubob <Ki28ryu@icloud.com>
2026-05-23 14:28:49 +0900

    [add]ユニットの見た目を管理するクラス

---
+++ Assets/Script/Unit/View/UnitView.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UnitView : MonoBehaviour
+{
+    public UnitInstance Instance;
+
+    public void Initialize(UnitInstance instance)
+    {
+        Instance = instance;
+    }
+}

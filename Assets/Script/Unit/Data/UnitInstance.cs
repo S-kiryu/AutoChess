25c0547fd50eac2555fc213feb1f7bf434eb8996
KINOKO\nubob <Ki28ryu@icloud.com>
2026-05-23 14:27:18 +0900

    [add]キャラの実態

---
+++ Assets/Script/Unit/Data/UnitInstance.cs
@@ -0,0 +1,16 @@
+﻿[System.Serializable]
+public class UnitInstance   //キャラの実態
+{
+    public string UniqueId;          // 個体ID
+    public CharacterData Data;       // ベース情報
+    private UnitStatus Status;        // ランタイムステータス
+
+    public UnitInstance(CharacterData data)
+    {
+        UniqueId = System.Guid.NewGuid().ToString();
+        Data = data;
+
+        Status = new UnitStatus();
+        Status.Initialize(data.BaseStatus);
+    }
+}

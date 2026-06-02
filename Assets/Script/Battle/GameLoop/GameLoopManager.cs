1771e5a9182577e6a8a3bec6d2fd408b43819ad7
KINOKO\nubob <Ki28ryu@icloud.com>
2026-06-02 13:07:45 +0900

    [add]ゲームの流れを管理するクラス

---
+++ Assets/Script/Battle/GameLoop/GameLoopManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class GameLoopManager : MonoBehaviour
+{
+    public GameState CurrentState { get; private set; }
+
+    public void ChangeState(GameState newState)
+    {
+        CurrentState = newState;
+
+        switch (newState)
+        {
+            case GameState.Preparation:
+                OnPreparation();
+                break;
+            case GameState.Battle:
+                OnBattle();
+                break;
+            case GameState.Reward:
+                OnReward();
+                break;
+        }
+    }
+
+    private void OnPreparation()
+    {
+        // 準備フェーズの処理
+        Debug.Log("準備フェーズに入りました。");
+    }
+
+    private void OnBattle()
+    {
+        // 戦闘フェーズの処理
+        Debug.Log("戦闘フェーズに入りました。");
+    }
+
+    private void OnReward()
+    {
+        // 報酬フェーズの処理
+        Debug.Log("報酬フェーズに入りました。");
+    }
+}

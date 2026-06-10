ec028380d9e8843f2cd975bffeb73be6d1a416e6
KINOKO\nubob <Ki28ryu@icloud.com>
2026-06-10 19:44:59 +0900

    [add]ベンチのハンドラー

--- Assets/Script/Bench/BenchSlot.cs
+++ Assets/Script/Bench/BenchSlot.cs
@@ -1,10 +1,27 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BenchSlot : MonoBehaviour,IDropHandler
+public class BenchSlot : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData) 
-    {
+    private int x;
+    private int y;
+    private BenchManager benchManager;
 
+    public int X => x;
+    public int Y => y;
+
+    public void Initialize(int x, int y, BenchManager benchManager)
+    {
+        this.x = x;
+        this.y = y;
+        this.benchManager = benchManager;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // 後でここにドラッグ中ユニットの受け取りを書く
+        // 例:
+        // UnitInstance unit = ...
+        // benchManager.SetUnit(unit, x, y);
     }
 }

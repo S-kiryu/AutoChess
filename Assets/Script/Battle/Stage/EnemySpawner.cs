bead7f220e871d00a2eaa8a555b9e8b4aaac2b80
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-06-16 14:57:49 +0900

    [add]指定した座標に敵ユニットを生成するクラス

---
+++ Assets/Script/Battle/Stage/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEngine;
+
+/// <summary>
+/// 戦闘時に敵を生成するクラス
+/// </summary>
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private BattleUnit enemyPrefab;
+    //[SerializeField] private BattleGrid battleGrid;
+    [SerializeField] private Transform enemyParent;
+
+    /// <summary>
+    /// ステージのデータを読み込み適したところにユニットを生成するクラス
+    /// </summary>
+    /// <param name="stageData">生成したいステージ</param>
+    /// <returns></returns>
+    public List<BattleUnit> SpawnEnemies(BattleStageData stageData)
+    {
+        List<BattleUnit> enemies = new List<BattleUnit>();
+
+        //適した位置に敵を生成している
+        foreach (EnemySpawnData spawnData in stageData.Enemies)
+        {
+            //Vector3 spawnPosition = battleGrid.GetWorldPosition(spawnData.GridPosition);
+            Vector3 spawnPosition = new Vector3();
+
+            BattleUnit enemy = Instantiate(
+                enemyPrefab,
+                spawnPosition,
+                Quaternion.identity,
+                enemyParent
+            );
+
+            UnitInstance enemyInstance = new UnitInstance();
+            enemyInstance.Initialize(spawnData.CharacterData);
+
+            enemy.Initialize(enemyInstance, teamId: 1);
+
+            enemies.Add(enemy);
+        }
+
+        return enemies;
+    }
+}
